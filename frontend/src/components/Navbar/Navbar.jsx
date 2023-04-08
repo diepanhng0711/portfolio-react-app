9131bea6dd10b37d4ae4a053328afe7f05600081
TueNt <diepanhng0711@gmail.com>
2023-04-08 11:52:03 +0700

    Finish Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
-      <ul className="app__navber-links">
+      <ul className="app__navbar-links">
         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
